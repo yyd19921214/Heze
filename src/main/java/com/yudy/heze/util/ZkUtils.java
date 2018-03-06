6a721bd2b5720946a409476f3161c6d65f361490
Yudong Yang <yudy@microsoft.com>
2018-03-06 21:05:49 +0800

    updates

---
+++ src/main/java/com/yudy/heze/util/ZkUtils.java
@@ -0,0 +1,13 @@
+package com.yudy.heze.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZkUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZkUtils.class);
+
+    public static final String ZK_MQ_BASE="/HEZEMQ";
+
+    
+
+}
