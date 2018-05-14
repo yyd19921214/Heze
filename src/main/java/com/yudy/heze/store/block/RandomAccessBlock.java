aab8b5de070b3376fe07fdff50afd86d464d377d
Yudong Yang <yudy@microsoft.com>
2018-05-14 20:44:18 +0800

    update

--- src/main/java/com/yudy/heze/store/block/RandomAccessBlock.java
+++ src/main/java/com/yudy/heze/store/block/RandomAccessBlock.java
@@ -1,11 +1,12 @@
 package com.yudy.heze.store.block;
 
-import com.yudy.heze.store.index.TopicQueueIndex;
 import com.yudy.heze.store.index.RandomAccessBlockIndex;
+import com.yudy.heze.store.queue.RandomAccessTopicQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.Cleaner;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.lang.reflect.Method;
@@ -15,100 +16,84 @@ import java.nio.channels.FileChannel;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 
-/**
- * 一种支持数据随机访问的文件结构
- * 利用Index完成对数据的定位
- * 具体实现见链接: https://tech.meituan.com/kafka-fs-design-theory.html
- */
-public class RandomAccessBlock extends AbstractQueueBlock {
+public class RandomAccessBlock {
 
     private final Logger LOG = LoggerFactory.getLogger(RandomAccessBlock.class);
 
-    public static final String BLOCK_FILE_SUFFIX = ".umq";//数据文件
-
-    private static final String BLOCK_FILE_PREFIX = "block";
-
     public static int BLOCK_SIZE = 32 * 1024 * 1024;//32MB
 
-    private String blockFilePath;
+    protected String blockFilePath;
+
     private RandomAccessBlockIndex index;
+
     private ByteBuffer byteBuffer;
+
     private MappedByteBuffer mappedBlock;
+
     private FileChannel fileChannel;
+
     private RandomAccessFile blockFile;
 
 
-
-    public RandomAccessBlock(String blockFilePath, TopicQueueIndex index, ByteBuffer byteBuffer, MappedByteBuffer mappedByteBuffer, FileChannel fileChannel, RandomAccessFile randomAccessFile) {
-        super(blockFilePath,index,byteBuffer,mappedByteBuffer,fileChannel,randomAccessFile);
+    public RandomAccessBlock(RandomAccessBlockIndex index, String fileDir) {
+        this.index = index;
+        this.blockFilePath = getBlockFilePath(index.getIndexName(), fileDir);
+        try {
+            File file = new File(this.blockFilePath);
+            this.blockFile = new RandomAccessFile(file, "rw");
+            this.fileChannel = this.blockFile.getChannel();
+            this.mappedBlock = this.fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, BLOCK_SIZE);
+            this.byteBuffer = this.mappedBlock.load();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException(e);
+        }
     }
 
-    public RandomAccessBlock(TopicQueueIndex index, String blockFilePath) {
-        super(index,blockFilePath);
+    private RandomAccessBlock(RandomAccessBlockIndex index, String blockFilePath, RandomAccessFile blockFile, FileChannel fileChannel, MappedByteBuffer mappedByteBuffer, ByteBuffer byteBuffer) {
+        this.index = index;
+        this.blockFilePath = blockFilePath;
+        this.blockFile = blockFile;
+        this.fileChannel = fileChannel;
+        this.mappedBlock = mappedByteBuffer;
+        this.byteBuffer = byteBuffer;
     }
 
-    public RandomAccessBlockIndex getIndex(){
+    public RandomAccessBlockIndex getIndex() {
         return this.index;
     }
 
     public RandomAccessBlock duplicate() {
-        //todo need to be refined
-        return new RandomAccessBlock(this.blockFilePath, this.index, this.byteBuffer.duplicate(), this.mappedBlock, this.fileChannel, this.blockFile);
+        return new RandomAccessBlock(this.index, this.blockFilePath, this.blockFile, this.fileChannel, this.mappedBlock, this.byteBuffer);
     }
 
-    @Override
-    public void putEOF() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
     public boolean isSpaceAvailable(int len) {
         int lastRecordPos=index.getLastRecordPosition();
         byteBuffer.getLong(lastRecordPos);
         int lastRecordLen=byteBuffer.getInt();
         int skipToPos=lastRecordPos+Long.BYTES+Integer.BYTES+lastRecordLen;
         int increment = len + Integer.BYTES +Long.BYTES;//len(offset)+len(len)+len(msg);
-        return BLOCK_SIZE >= increment + skipToPos + 4;
+        return BLOCK_SIZE >= increment + skipToPos;
     }
-
-    @Override
-    public boolean eof() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * the structure of single message is like this
-     * ----------------
-     * offset(long)
-     * len of message(int)
-     * contents of message(bytes)
-     * ----------------
-     * @param bytes
-     * @return
-     */
-//    @Override
-//    public int write(byte[] bytes) {
-//        int pos=index.getLastRecordPosition();
-//        long offset=byteBuffer.getLong(pos);
-//        int len=byteBuffer.getInt();
-//        int skipToPos=pos+Long.BYTES+Integer.BYTES+len;
-//        byteBuffer.position(skipToPos);
-//        byteBuffer.putLong(offset+1);
-//        byteBuffer.putInt(bytes.length);
-//        byteBuffer.put(bytes);
-//        index.updateIndex(offset+1,skipToPos);
-//        return skipToPos;
-//    }
 
     public long write(byte[] bytes) {
-        //todo
-        // write the message and return the offset of this message
-        return 1L;
+        int lastRecordPos=index.getLastRecordPosition();
+        long lastRecordOffset=index.getLastOffset();
+        long rtnOffset=lastRecordOffset+1;
+        byteBuffer.position(lastRecordPos);
+        byteBuffer.getLong();
+        int len=byteBuffer.getInt();
+        int skipToPos= lastRecordPos+Long.BYTES+Integer.BYTES+len;
+        byteBuffer.position(skipToPos);
+        byteBuffer.putLong(rtnOffset);
+        byteBuffer.putInt(bytes.length);
+        byteBuffer.put(bytes);
+        //todo refine the detail of update index
+        index.updateIndex(rtnOffset,skipToPos);
+        return rtnOffset;
     }
 
-
-    @Override
-    public byte[] read(int offset) {
+    public byte[] read(long offset) {
         int pos=this.index.getReadPosition(offset);
         byteBuffer.position(pos);
         byteBuffer.getLong();
@@ -118,38 +103,11 @@ public class RandomAccessBlock extends AbstractQueueBlock {
         return bytes;
     }
 
-    public byte[] read(long offset) {
-        //todo
-        return null;
-//        int pos=this.index.getReadPosition(offset);
-//        byteBuffer.position(pos);
-//        byteBuffer.getLong();
-//        int len=byteBuffer.getInt();
-//        byte[] bytes=new byte[len];
-//        byteBuffer.get(bytes);
-//        return bytes;
-    }
-
-    @Override
-    public int countRecord() {
-        return index.getTotalNum();
-    }
-
-    @Override
-    public byte[] read() {
-        throw new UnsupportedOperationException();
-    }
-
-
-
-    @Override
     public void sync() {
         if (mappedBlock != null)
             mappedBlock.force();
-
     }
 
-    @Override
     public void close() {
         try {
             if (mappedBlock == null) {
@@ -178,5 +136,21 @@ public class RandomAccessBlock extends AbstractQueueBlock {
         }
     }
 
+
+
+    /**
+     * the index name is like: index_{queueName}_{first offset of this index}.umq
+     * the block name if like: block_{queueName}_{first offset of this block}.umq
+     *
+     * @param indexName
+     * @param fileDir
+     * @return
+     */
+    private String getBlockFilePath(String indexName, String fileDir) {
+        String blockName = RandomAccessTopicQueue.BLOCK_PREFIX + indexName.substring(4);
+        String blockFilePath = fileDir + File.separator + blockName;
+        return blockFilePath;
+
+    }
 
 }
